91aae3b60e1d39dfc005de833fdd2caa51eb6d46
l0u0h0 <l0u0h0@gmail.com>
2023-12-09 21:02:42 +0900

    :sparkles: [BE] 코드 삭제

--- backend/dksl/src/main/java/com/ssafy/dksl/model/repository/LbtiAnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.dksl.model.repository;
-
-import com.ssafy.dksl.model.entity.LbtiAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LbtiAnswerRepository extends JpaRepository<LbtiAnswer, Long> {
-}
