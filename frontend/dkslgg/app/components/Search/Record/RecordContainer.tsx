04c357c6458202722b2fdd1a28a89c37a8542a21
l0u0h0 <l0u0h0@gmail.com>
2024-08-26 16:49:05 +0900

    :lipstick: Rank Type Radio 스타일 수정

--- frontend/dkslgg/app/components/Search/Record/RecordContainer.tsx
+++ frontend/dkslgg/app/components/Search/Record/RecordContainer.tsx
@@ -7,7 +7,7 @@ export default function RecordContainer() {
   return (
     <div className="w-full px-[20%] py-16 bg-zinc-200 dark:bg-zinc-800">
       <div className="w-full min-w-[1024px] flex justify-end pb-2">
-        <div className="w-[70%] flex gap-[.25rem] pl-4">
+        <div className="w-[70%] flex gap-[.25rem] pl-4 items-center custom-radio font-semibold text-zinc-700 dark:text-zinc-200">
           <input type="radio" name="rank-type" defaultChecked />
           <label className="mr-2">랭크 전체</label>
           <input type="radio" name="rank-type" />
