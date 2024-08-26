b630f08e58ddb39b9069ad2fe266527d28a1dc38
l0u0h0 <l0u0h0@gmail.com>
2024-08-26 17:03:37 +0900

    :lipstick: Button 스타일 수정

--- frontend/dkslgg/app/components/Common/TabContainer.tsx
+++ frontend/dkslgg/app/components/Common/TabContainer.tsx
@@ -23,7 +23,7 @@ export default function TabContainer({
         {items.map((e, i) =>
           tab !== i ? (
             <button
-              className="px-6 py-2 rounded-lg"
+              className="px-6 py-2 rounded-md"
               key={`tab_key-${i}`}
               onClick={tabIndexHandler(i)}
             >
@@ -31,7 +31,7 @@ export default function TabContainer({
             </button>
           ) : (
             <button
-              className="bg-zinc-700 dark:bg-zinc-200 text-zinc-100 dark:text-zinc-700 px-6 py-2 rounded-lg"
+              className="bg-zinc-700 dark:bg-zinc-200 text-zinc-100 dark:text-zinc-700 px-6 py-2 rounded-md"
               key={`tab_key-${i}`}
               disabled
             >
