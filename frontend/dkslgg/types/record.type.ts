8f5c0604f98ea2421bb4d7a3f769b51e02e1f310
l0u0h0 <l0u0h0@gmail.com>
2024-08-30 21:48:33 +0900

    :sparkles: Data Type 추가

--- frontend/dkslgg/types/record.type.ts
+++ frontend/dkslgg/types/record.type.ts
@@ -36,3 +36,114 @@ export type OriginProfileDataType = {
     }
   ];
 };
+
+export type RecentAnalyzeDataType = {
+  win: number;
+  lose: number;
+  kill: number;
+  death: number;
+  assist: number;
+  kda: number;
+  kill_participation: number;
+  champions: [
+    {
+      name: string;
+      odds: number;
+      kda: number;
+      games: number;
+    }
+  ];
+  positions: [
+    {
+      percent: number;
+      name: string;
+    }
+  ];
+};
+
+export type DuoPlayerDataType = {
+  name: string;
+  game: number;
+  win: number;
+  lose: number;
+  odds: number;
+};
+
+export type RankDataType = {
+  solo_tier: string;
+  free_tier: string;
+  tft_tier: string;
+};
+
+export type RecordDataType = {
+  win_or_lose: number; // 0: win, 1: lose
+  game_type: number;
+  game_duration: string; // 게임 진행 시간
+  game_time: string; // ex_1시간 전.
+  champ_name: string;
+  level: number;
+  spell_0_id: string;
+  spell_1_id: string;
+  rune_0_id: string;
+  rune_1_id: string;
+  kill: number;
+  death: number;
+  assist: number;
+  kda: number;
+  position: string;
+  kill_participation: number;
+  cs: number;
+  vision: number;
+  item_0_id: number;
+  item_1_id: number;
+  item_2_id: number;
+  item_3_id: number;
+  item_4_id: number;
+  item_5_id: number;
+  item_6_id: number;
+  teams: [
+    {
+      win_or_lose: number;
+      players: [
+        {
+          name: string;
+          champ_name: string;
+          level: number;
+          spell_0_id: string;
+          spell_1_id: string;
+          rune_0_id: string;
+          rune_1_id: string;
+          kill: number;
+          death: number;
+          assist: number;
+          kda: number;
+          position: string;
+          gold: number;
+          cs: number;
+          vision: number;
+          total_damage_dealt_to_champions: number; // 딜량
+          damage_taken_on_team_percentage: number; // 딜퍼센테이지
+          item_0_id: number;
+          item_1_id: number;
+          item_2_id: number;
+          item_3_id: number;
+          item_4_id: number;
+          item_5_id: number;
+          item_6_id: number;
+        }
+      ];
+    }
+  ];
+};
+
+export type RecordContainerType = {
+  rank: RankDataType;
+  duo_player: DuoPlayerDataType[];
+  recent_analyze: RecentAnalyzeDataType;
+  record: RecordDataType[];
+};
+
+export type GetRecordDataType = {
+  profile: ProfileDataType;
+  record: RecordContainerType;
+};
